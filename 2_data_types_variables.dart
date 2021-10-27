46f9e121221c8b0757edd1cce8734be349088259
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-10-28 03:07:06 +0530

    Create 2_data_types_variables.dart

---
+++ 2_data_types_variables.dart
@@ -0,0 +1,37 @@
+void main() {
+  
+ // int 
+  
+  int age = 10;
+  
+  //var int age = 10;
+  
+  print(age);
+  
+  // double 
+  
+  double percent = 98.3 ; 
+  
+  //var percent = 98.3 ; 
+
+  print(percent);
+  
+  // strings 
+  
+  String name = "henry";
+  
+  //var name = "henry";
+  
+  print(name);
+  
+  //boolean 
+  
+  bool isEven = true;
+  
+  //var isEven = true;
+
+  print(isEven);
+  
+  //null safety in dart 
+  
+}
