0ea1aba8dee9d2c9419b7ff91c04b3f34385eb54
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-10-27 12:23:24 +0530

    Update 1_hello_world.dart

--- 1_hello_world.dart
+++ 1_hello_world.dart
@@ -1,8 +1,12 @@
 void main() {
 
+  // this is my first program for demo 
+  
 print("hello world");
 
 print("this is my first program");
+  
+  // this is maths operation
 
 print(4);
 
@@ -10,6 +14,7 @@ print(5-3);
 
 print(8/4);
 
+  // printing out boolean value 
 print(true);
 
 }
