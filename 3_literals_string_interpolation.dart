093c3fc748a8682b31b6b0de35f7bb8aa9e847de
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-10-28 14:32:56 +0530

    Create 3_literals_string_interpolation.dart

---
+++ 3_literals_string_interpolation.dart
@@ -0,0 +1,47 @@
+void main() {
+  
+  // literal 
+  
+  var a = 5;
+  double v = 5.3;
+  String abc = "henry";  
+  
+  //various types to write literals 
+  
+  String s = "henry";
+  String s1 = 'henry';
+  
+  String s2 = "It's Okay";
+  
+  String s3 = 'It\'s Okay';
+  
+  String s4 = "this is my first example for string interpolation"
+              "hello guys subscribe my channel";
+  
+  
+  //string interpolation
+  
+  String name = "henry";
+  
+  //String name1 = "newton" + name ;
+  
+  String name1 = "newton $name";
+  
+  print(name1);
+  
+  //area of rectangle 
+  
+  int l = 10;
+  int b = 20;
+  
+  print("area of rectangle ${l*b}");
+
+  
+  //operation on string
+  
+  print(name.length);
+  print(name.toLowerCase());
+  
+  //many more example will 
+  
+}
