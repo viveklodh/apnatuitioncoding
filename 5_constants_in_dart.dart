19a1a88d959485a3b216615ead593f9d64678ebb
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-01 19:44:28 +0530

    Create 5_constants_in_dart.dart

---
+++ 5_constants_in_dart.dart
@@ -0,0 +1,28 @@
+void main() {
+  
+  //final keyword 
+  
+  final int a = 10;
+  
+  //a = 30;
+  
+  //when used then uses memory
+  
+  print(a);
+  
+  //const keyword 
+  
+  const int b = 20;
+  
+  //when compile then uses memory
+  
+}
+class ABC {
+  
+  final int a = 10;
+  
+  static const int ab = 20;
+  
+}
+  
+  
