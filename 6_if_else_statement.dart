e4e2a8aea3b13032120f3f07e90791c1609882dc
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-02 21:39:19 +0530

    Create 6_if_else_statement.dart

---
+++ 6_if_else_statement.dart
@@ -0,0 +1,34 @@
+
+void main(){
+  
+  
+  // if else statements 
+  
+  // number is positive or not
+  
+//   int number  = 40 ;
+  
+//   if (number < 0) {
+    
+//     print("$number is negative");
+//   }
+//   else {
+    
+//     print("$number is positive");
+//   }
+  
+  //largest number in two
+  
+  int a = 50 ;
+  
+  int b = 20;
+  
+  if ( a > b ) {
+    
+     print("$a is larger ");
+  }
+  else 
+  {
+    print("$b is larger");
+  }
+}
