851ec1ae1917a99b278d75e134de1c5938767e1f
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-03 02:24:55 +0530

    Update 6_if_else_statement.dart

--- 6_if_else_statement.dart
+++ 6_if_else_statement.dart
@@ -6,6 +6,23 @@ void main() {
   
   // if - else statements 
   
+  //number is positive or not
+  
+//   int number = 100;
+  
+//   if ( number > 0 ) {
+   
+//     print("$number is positive");
+    
+//   }
+//   else {
+   
+//     print("$number is negative");
+    
+//   }
+  
+  // larger in two numbers 
+  
   int number1 = 200;
   
   int number2 = 400;
