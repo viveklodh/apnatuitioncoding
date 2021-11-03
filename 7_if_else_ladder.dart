6326217202cca3040da7f3d48e16c3833503642a
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-03 13:27:14 +0530

    Update 7_if_else_ladder.dart

--- 7_if_else_ladder.dart
+++ 7_if_else_ladder.dart
@@ -1,25 +1,38 @@
-// conditional / ternary operator 
+// if else ladder statement
+
+/*
+  
+  marks        grade 
+  
+  80 - 100     A
+  60 =< 80      B
+  35 =< 60      C
+  0 < 35       Fail
+  
+  */
 
 void main() {
   
-
-  //syntax
+  int marks = 101;
   
-  // (condition)? stmt1 : stmt2 ;
-  
-//   int number = -5;
-  
-//   (number>0)? print("number is positive") : print("number is negative");
-  
-  int a = 50;
-  int b = 10;
-  
-  int large = 0;
-  
-   large = (a>b)? a : b ;
-  
-  print(large);
- 
-  
+   if (marks >= 80 && marks <= 100)  
+   {
+     print("A");
+   }
+  else if (marks >= 60 && marks < 80)
+  {
+    print("B");
+  }
+  else if (marks >= 35 && marks < 60)
+  {
+    print("C");
+  }
+  else if (marks >= 0 && marks < 35 )
+  {
+    print("fail");
+  }  
+  else {
+    print("invalid marks")
+  }
 }
 
