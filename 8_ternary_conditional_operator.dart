dff07bdea64f27185ef4a7896450380246f56670
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-03 13:26:42 +0530

    Create 8_ternary_conditional_operator.dart

---
+++ 8_ternary_conditional_operator.dart
@@ -0,0 +1,25 @@
+// conditional / ternary operator 
+
+void main() {
+  
+
+  //syntax
+  
+  // (condition)? stmt1 : stmt2 ;
+  
+//   int number = -5;
+  
+//   (number>0)? print("number is positive") : print("number is negative");
+  
+  int a = 50;
+  int b = 10;
+  
+  int large = 0;
+  
+   large = (a>b)? a : b ;
+  
+  print(large);
+ 
+  
+}
+
