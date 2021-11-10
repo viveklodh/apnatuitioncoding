1c160cc4a267d62495b3a8973bb6867e39d55b1e
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-11 00:50:25 +0530

    Create 10_incremen_decrement_operator.dart

---
+++ 10_incremen_decrement_operator.dart
@@ -0,0 +1,58 @@
+
+void main()
+{
+  
+//pre increment operator
+  
+ 
+  
+  int a ;
+  int b = 5;
+  
+  a = ++b;
+  
+  print(a);   //output 6
+  print(b);   //output 6
+  
+ 
+  //post increment operator
+  
+  /*
+  
+  int a ;
+  int b = 5;
+  
+  a = b++;
+  
+  print(a);   //output 5
+  print(b);   //output 6
+  
+  */
+  //pre decrement operator
+  
+  /*
+  
+  int a ;
+  int b = 5;
+  
+  a = --b;
+  
+  print(a);   //output 4
+  print(b);   //output 4
+  
+  */
+  //post decrement operator
+  
+  /*
+  
+  int a ;
+  int b = 5;
+  
+  a = b--;
+  
+  print(a);   //output 5
+  print(b);   //output 4
+  
+  */
+  
+}
