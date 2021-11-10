7c6ccf13778edaca348a587c296d7783ead46976
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-11 00:59:00 +0530

    Create 11_for_loop.dart

---
+++ 11_for_loop.dart
@@ -0,0 +1,40 @@
+
+void main() {
+  
+  //hello 5 times
+  
+  for(int i = 1 ; i <= 5 ; i++ ) 
+  {
+    print("hello");
+  }
+  
+  //number 1 to 10
+  
+   for(int i = 1 ; i <= 10 ; i++ ) 
+  {
+    print(i);
+  }
+  
+  // even number  1 to 10
+  
+   for(int i = 1 ; i <= 5 ; i++ ) 
+  {
+     if (i%2 == 0)
+     {
+    print("hello");
+     }
+     
+  }
+    // odd number  1 to 10
+  
+   for(int i = 1 ; i <= 5 ; i++ ) 
+  {
+     if (i%2 != 0)
+     {
+    print("hello");
+     }
+     
+  }
+  
+  
+}
