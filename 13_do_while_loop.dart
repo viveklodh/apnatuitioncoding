d2f02ebfcd20d686e03662ef333b91f8f13317dc
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-13 16:44:24 +0530

    Create 13_do_while_loop.dart

---
+++ 13_do_while_loop.dart
@@ -0,0 +1,23 @@
+void main() {
+  
+  //print hello 5 times 
+  
+  int i = 1;
+  
+  do {
+    print("hello");
+    i++;
+  }
+  while(i<=5);
+  
+   //print number 1 to 10
+  
+//   int i = 1;
+  
+//   do {
+//     print(i);
+//     i++;
+//   }
+//   while(i<=10);
+  
+}
