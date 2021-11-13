ff1454e792ad92d6df96a544b569601d9d1d5c67
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-13 16:41:18 +0530

    Create 12_while_loop.dart

---
+++ 12_while_loop.dart
@@ -0,0 +1,38 @@
+void main()
+{
+  
+  //printing hello 5 times 
+  
+  int i = 1 ;
+  
+  while(i<=5) {
+    
+    print("hello");
+    i++;
+  }
+  
+   //printing number  
+  
+//   int i = 1 ;
+  
+//   while(i<=5) {
+    
+//     print(i);
+//     i++;
+//   }
+  
+    //printing even number upto 10
+  
+//   int i = 1 ;
+  
+//   while(i<=10) {
+    
+//     if (i%2 == 0)
+//        { 
+//         print(i);
+//        }
+  
+//     i++;
+//   }
+  
+}
