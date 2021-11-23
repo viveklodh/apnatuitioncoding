b84ffd636d91aa161cc31b9a6da532f26e25ccc2
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2021-11-23 14:51:21 +0530

    Create 18_optional_positional_parameter.dart

---
+++ 18_optional_positional_parameter.dart
@@ -0,0 +1,17 @@
+//optional positional parameter 
+
+
+void main() {
+  
+  cityName('mumbai','pune');
+  
+}
+
+
+void cityName(String a , [String b ,String c]){
+  
+    print("first city $a");
+    print("second city $b");
+    print("third city $c");
+  
+}
