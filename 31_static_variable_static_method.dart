8186c16df426a49dd7d0dc0cb39c806b483ee4e0
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2022-05-05 14:50:17 +0530

    Create 31_static_variable_static_method.dart

---
+++ 31_static_variable_static_method.dart
@@ -0,0 +1,25 @@
+
+// static methods and variables 
+
+void main(){
+  
+Circle circle = Circle();
+  circle.myNormalFunction();
+    
+}
+
+class Circle {
+  
+ static const double pi = 3.14;
+    
+ static void calculateArea(){
+    print("calculate area");
+  }
+  
+  myNormalFunction()
+  {
+    Circle.calculateArea();
+    print("normal function");
+  }
+  
+  }
