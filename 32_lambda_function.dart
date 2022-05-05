d2bc369ae470aba3bfa46c33df15f569e2d6532f
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2022-05-05 14:51:11 +0530

    Create 32_lambda_function.dart

---
+++ 32_lambda_function.dart
@@ -0,0 +1,28 @@
+ 
+//lambda function , anonymous function
+// without name 
+
+void main(){
+  
+Function summation =   (int a , int b){
+  int sum = a+b;
+  print(sum);
+};
+  
+  summation(1,2);
+  
+  Function square = (int n){
+    
+    return n * n;
+    
+  };
+  
+  print(square(4));
+  
+}
+
+// addTwoNumbers(int a , int b){
+//   int sum = a+b;
+//   print(sum);
+// }
+
