9e0b5aa7aa28138c2b2a2bd6694a195c5d694cb6
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2022-05-05 14:59:01 +0530

    Create 37_map.dart

---
+++ 37_map.dart
@@ -0,0 +1,29 @@
+
+
+// map  // hash // dictionary 
+
+// unordered collection of key value pairs 
+
+void main(){
+  
+  
+  Map<String,int> numbers = {
+    "one":1,
+    "two":2,
+    "three":3
+  };
+  
+//  Map<String,String> name = Map(); 
+  
+//   name["karan"] = "vivek";
+//   name["nitish"]="abhishek";
+  
+//   print(name);
+  
+//   print(name["karan"]);
+  
+//  print( name.containsKey("karan"));
+  
+//   name.forEach((key,value)=>print("$key and $value"));
+ 
+}
