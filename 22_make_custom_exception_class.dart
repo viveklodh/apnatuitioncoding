5868ec719dc5753fd315ad263a4cf77bb677adc2
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2022-07-24 17:34:38 +0530

    Update 22_make_custom_exception_class.dart

--- 22_make_custom_exception_class.dart
+++ 22_make_custom_exception_class.dart
@@ -5,30 +5,39 @@
 // 4. Finally Clause
 // 5. Create our own Custom Exception
 
-void main() {
+//how to make your own custom exception class 
+
+class AmountException implements Exception{
   
-  int amount = -10 ;
-  
-  try{
-    
-  if (amount < 0){
-    throw AmountException();
+  errorMsg(){
+    return "please enter greater than 0 rs";
   }
-    
-  }
-    catch(e){
-      
-      print(e.errorMsg());
-      
-    }
-  
   
 }
 
-class AmountException implements Exception {
+
+void main(){
   
-  errorMsg(){
-    return "please enter greater than 0 amt.";
+  try{
+  withdraw(-10);
   }
+  on AmountException catch(e){
+    print(e.errorMsg());
+  }
+  finally{
+    print("ending transactions");
+  }
+  
+}
+
+withdraw(int amt){
+  
+  if(amt<0){
+    throw AmountException();
+  }
+  else{
+    print(amt);
+  }
+  
   
 }
