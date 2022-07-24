5836de932594bc44a5c89ac749ea38203bd14a86
vivek lodh <75872299+viveklodh@users.noreply.github.com>
2022-07-24 23:22:43 +0530

    Update 38_mixin.dart

--- 38_mixin.dart
+++ 38_mixin.dart
@@ -1,15 +1,23 @@
-//mixin
+// mixin in dart 
 
 void main(){
-  Student s =Student();
-  s.studentRule();
-}
-mixin StudentRule{
-  studentRule(){}
-}
-class College{
-  rules(){}
-}
-class Student extends College with StudentRule{}
-class Teacher extends College{}
+ Doctor b = Doctor();
+  b.takeTemp();
 
+  
+}
+mixin Medical {
+      takeTemp(){}
+}
+class Employee{
+    clockIn(){}
+}
+
+class Doctor extends Employee with Medical{
+  operation(){}
+}
+class Nurse extends Employee with Medical {
+  takeCarePatient(){}
+}
+class Bartender extends Employee{
+}
